07bc5d965f80997b642e4f0cde45757a7a287d7d
simon <28737164@qq.com>
2015-03-13 17:19:28 +0800

    修改备注，增加规范

--- src/Traits/Fields.php
+++ src/Traits/Fields.php
@@ -3,27 +3,16 @@ namespace Fields\Traits;
 trait Fields {
 	
 	/**
-	 * 字段数值值-主要用于show的时候在callable中调用返回即setFieldsCallable中使用
+	 * 请求数据
 	 * @var array
 	 */
-// 	protected static $fieldsData = array();
-	
 	protected static $requestData = array();
 	
-// 	/**
-// 	 * 字段abstract函数，用于设置字段规则
-// 	 */
-// 	abstract public function fields();
-	
 	/**
-	 * 设置字段数据值
+	 * 设置请求数据
 	 * @param array $data
+	 * @return array
 	 */
-// 	public function setFieldsData(array $data) {
-// 		self::$fieldsData = $data;
-// 		return $this;
-// 	}
-	
 	public static function setRequestData(array $data) {
 		self::$requestData = $data;
 		return $data;
@@ -42,34 +31,11 @@ trait Fields {
 				$value = $this->resolveFieldsRule($value);
 			} elseif (is_callable($value)) {//callable
 				$value = call_user_func($value);
-// 				dd($value);exit();
-// 				\ReflectionParameter::export($function, $parameter)
-// 				$ReflectionFunction = new \ReflectionFunction($value);
-// 				$params = array();
-// 				foreach ($ReflectionFunction->getParameters() as $param) {
-// 					echo 'param name: ' . $param->getName(),"\n";
-// // 					dd( $param->isOptional());
-// // 					if ($param->isOptional()) {
-// 						echo 'Default value: ' . $param->getValue(),"\n";
-// // 					}
-// 				}
-// 				exit();
-// 				foreach ($ReflectionFunction->getParameters() as $param) {
-// 					$params[] = $param;
-					
-					
-// 				}
-// 				dd($params);exit();
-// 				dd($ReflectionFunction->getParameters()[0]->reflection);
-// 				$value = call_user_func_array($value, $ReflectionFunction->getParameters());
-// 				print_r($value);exit();
-				
 			} elseif (method_exists($this, $method = "{$key}FieldRule")) {//method_Exists
 				$value = $this->$method();
 			}
 			
 		}
-// 		print_r($array);exit();
 		return $array;
 	}
 	
